bba5b5131e58509338e9ec84b67565a60185747c
GatorBait2626 <gatorbait2626@GatorBait2626s-MacBook-Air.local>
2026-02-27 14:35:33 -0500

    Day 5 JavaScript Variables and Data Types Practice

---
+++ variables.js
@@ -0,0 +1,12 @@
+const course = "30 Days to Automation";
+let testerName = "Eric";
+let isEnrolled = true;
+let tools = ["Git", "VS Code", "Playwright"];
+let profile = { name: testerName, course: course, tools: tools };
+
+console.log("Welcome " + testerName + " to " + course);
+console.log("Enrolled:", isEnrolled);
+console.log("Favorite Tool:", tools[1]);
+console.log("Profile:", profile);
+ 
+
